772c806bc94a0560349399b6ea14a9dedf0b299d
고범석 <kobums@naver.com>
2026-02-24 23:34:43 +0900

    feat(config): API 프록시 리라이트 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://snippetapi.gowoobro.com/api/:path*",
+      },
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8008/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
